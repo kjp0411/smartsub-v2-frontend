5d12e58a76d0f42682c3b1ee9cff3886748df959
kjp0411 <98109773+kjp0411@users.noreply.github.com>
2026-07-08 00:24:56 +0900

    fix: update api base url for production

--- src/api/client.js
+++ src/api/client.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api/v1',
+  baseURL: 'https://api.smartsub.dev/api/v1',
 });
 
 client.interceptors.request.use((config) => {
